34026d4b9a5841ce3a9d955bdce35be71216e31c
Nathan <nathanraysheryak@gmail.com>
2022-01-03 09:44:30 -0500

    basic knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      host: "localhost",
+      database: "budgeter",
+      user: "postgres",
+      password: "2345",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
