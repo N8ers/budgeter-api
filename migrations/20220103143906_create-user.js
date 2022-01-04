6cabdb467066fdf717a63b0d2ca8cad57d9534e2
Nathan <nathanraysheryak@gmail.com>
2022-01-04 06:34:10 -0500

    create vendor migration

--- migrations/20220103143906_create-user.js
+++ migrations/20220103143906_create-user.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable("user", function (table) {
-    table.increments("id");
+    table.increments("id").primary();
     table.string("name", 225).notNullable();
   });
 };
---
+++ migrations/20220104112023_create-vendor.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("vendor", function (table) {
+    table.increments("id").primary();
+    table.string("name", 225);
+    table.integer("user_id").unsigned().notNullable();
+
+    table.foreign("user_id").references("id").inTable("user");
+  });
+};
+
+exports.down = function (knex) {};
