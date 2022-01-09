a04e67b7ddac1f03c977cfccc7a6cb7bd307ff27
nathan sheryak <nathanraysheryak@gmail.com>
2022-01-09 06:32:26 -0500

    return created users after running seed

--- seeds/basic_seed.js
+++ seeds/basic_seed.js
@@ -1,13 +1,12 @@
-exports.seed = function (knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  return knex("user")
-    .del()
-    .then(function () {
-      // Inserts seed entries
-      return knex("user").insert([
-        { name: "Tsuki" },
-        { name: "Goon" },
-        { name: "Joe" },
-      ]);
-    });
+  await knex("user").del()
+
+  const users = await knex("user").returning(['id', 'name']).insert([
+    { name: "Tsuki" },
+    { name: "Goon" },
+    { name: "Joe" },
+  ])
+
+  console.log('generated users ', users)
 };
