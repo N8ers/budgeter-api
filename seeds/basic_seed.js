9f5c435da15df14506ae9368de56b5a0643d08c8
nathan sheryak <nathanraysheryak@gmail.com>
2022-01-10 07:34:44 -0500

    add vendors to seed

--- seeds/basic_seed.js
+++ seeds/basic_seed.js
@@ -1,12 +1,20 @@
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex("user").del()
+  await knex("user").del();
+  const users = await knex("user")
+    .returning(["id", "name"])
+    .insert([{ name: "Tsuki" }, { name: "Goon" }, { name: "Joe" }]);
 
-  const users = await knex("user").returning(['id', 'name']).insert([
-    { name: "Tsuki" },
-    { name: "Goon" },
-    { name: "Joe" },
-  ])
+  console.log("generated users ", users);
 
-  console.log('generated users ', users)
+  await knex("vendor").del();
+  const vendors = await knex("vendor")
+    .returning(["id", "name", "user_id"])
+    .insert([
+      { name: "Whole Foods", user_id: users[1].id },
+      { name: "Lush", user_id: users[1].id },
+      { name: "Adidas", user_id: users[2].id },
+    ]);
+
+  console.log("generated vendors ", vendors);
 };
