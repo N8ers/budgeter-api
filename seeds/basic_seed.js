dafb2fa7c34dd1c7b5e2d08dcce2f1f7cd26da44
nathan sheryak <nathanraysheryak@gmail.com>
2022-01-11 07:38:03 -0500

    add expense to seed

--- seeds/basic_seed.js
+++ seeds/basic_seed.js
@@ -1,20 +1,62 @@
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
+  await knex("expense").del();
+  await knex("vendor").del();
   await knex("user").del();
+
   const users = await knex("user")
     .returning(["id", "name"])
     .insert([{ name: "Tsuki" }, { name: "Goon" }, { name: "Joe" }]);
 
-  console.log("generated users ", users);
+  console.log("generated users: ", users);
 
-  await knex("vendor").del();
   const vendors = await knex("vendor")
     .returning(["id", "name", "user_id"])
     .insert([
       { name: "Whole Foods", user_id: users[1].id },
       { name: "Lush", user_id: users[1].id },
       { name: "Adidas", user_id: users[2].id },
+      { name: "Apple", user_id: users[2].id },
     ]);
 
-  console.log("generated vendors ", vendors);
+  console.log("generated vendors: ", vendors);
+
+  const expenses = await knex("expense")
+    .returning("*")
+    .insert([
+      {
+        description: "mac-mini",
+        type: "other",
+        date: new Date("Jan 8, 2022").toISOString(),
+        ammount: 769.0,
+        user_id: users[2].id,
+        vendor_id: vendors[3].id,
+      },
+      {
+        description: "bath bombs",
+        type: "health",
+        date: new Date("Jan 8, 2022").toISOString(),
+        ammount: 769.0,
+        user_id: users[1].id,
+        vendor_id: vendors[1].id,
+      },
+      {
+        description: "general groceries",
+        type: "groceries",
+        date: new Date("Jan 2, 2022").toISOString(),
+        ammount: 769.0,
+        user_id: users[1].id,
+        vendor_id: vendors[0].id,
+      },
+      {
+        description: "new running shorts",
+        type: "clothes",
+        date: new Date("Jan 2, 2022").toISOString(),
+        ammount: 769.0,
+        user_id: users[2].id,
+        vendor_id: vendors[2].id,
+      },
+    ]);
+
+  console.log("generated expenses: ", expenses);
 };
