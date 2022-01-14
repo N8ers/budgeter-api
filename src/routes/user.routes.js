ae1bd3c7a3791a05f27b7ae9c43cf71b7f8f3cbb
nathan sheryak <nathanraysheryak@gmail.com>
2022-01-14 07:16:15 -0500

    GET allUsers route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,14 @@
+const knex = require("../../config/config");
+
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   res.status(200).send("user routes work!");
+});
+
+router.get("/allUsers", async (req, res) => {
+  const result = await knex.select("*").from("user");
+  res.status(200).send(result);
 });
 
 module.exports = router;
