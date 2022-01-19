f9a0c80f7227ef4b9129600bebaea088515304ed
nathan sheryak <nathanraysheryak@gmail.com>
2022-01-19 07:50:51 -0500

    scaffolding for querying by date range

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -15,6 +15,46 @@ router.get("/:id", async (req, res) => {
   res.status(200).send(result);
 });
 
+// Get User Expenses
+router.get("/:id/expenses", async (req, res) => {
+  const userId = req.params.id;
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
+
+  // Abstract this middleware/validation stuff
+  if ((startDate && !endDate) || (!startDate && endDate)) {
+    res
+      .status(400)
+      .send(
+        "You must specify a startDate and endDate if you want to filter by date."
+      );
+  }
+
+  if (startDate && endDate) {
+    // validate date format
+  }
+
+  if (startDate && endDate) {
+    // probably put this in a controller too?
+    // const userExpenses = await knex
+    //   .select("*")
+    //   .from("expense")
+    //   .where({ user_id: userId })
+    //   .where("date", ">=", startDate)
+    //   .where("date", "<=", endDate);
+    // console.log("userExpenses ", userExpenses);
+  }
+
+  // put this in a controller?
+  const userExpenses = await knex
+    .select("*")
+    .from("expense")
+    .where({ user_id: userId });
+  console.log("userExpenses ", userExpenses);
+
+  res.status(200).send(userExpenses);
+});
+
 // get user/:id/expenses?sort/filter/etc
 
 module.exports = router;
