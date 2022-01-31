f23ed196b56c7bf5e006c7e749f12a7e3c05b0e4
nathan sheryak <nathanraysheryak@gmail.com>
2022-01-31 07:38:00 -0500

    start designing for sorting expense by date

--- src/routes/expense.routes.js
+++ src/routes/expense.routes.js
@@ -2,7 +2,22 @@ const knex = require("../../config/config");
 
 const router = require("express").Router();
 
-router.get("/", async (req, res) => {
+const validateSortQueryParam = function (req, res, next) {
+  // we only care about sorting by date right now
+  // need to research more about asc/desc soryBy url formatting
+  // ex) /expenses?sortBy=date:asc/desc ???
+
+  const sortBy = req?.query?.sortBy;
+
+  if (sort && !(sort === "asc" || sort === "desc")) {
+    console.log("You may only sort on `asc` and `desc`");
+  }
+
+  console.log({ sort });
+  return next();
+};
+
+router.get("/", validateSortQueryParam, async (req, res) => {
   const result = await knex.select("*").from("expense");
   res.status(200).send(result);
 });
