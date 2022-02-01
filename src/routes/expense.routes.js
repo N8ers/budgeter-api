eabbd71feb00856a2dce37d67e8fcb3b387832ac
nathan sheryak <nathanraysheryak@gmail.com>
2022-02-01 07:48:18 -0500

    adopt sortBy orderBy pattern

--- src/routes/expense.routes.js
+++ src/routes/expense.routes.js
@@ -3,22 +3,46 @@ const knex = require("../../config/config");
 const router = require("express").Router();
 
 const validateSortQueryParam = function (req, res, next) {
-  // we only care about sorting by date right now
-  // need to research more about asc/desc soryBy url formatting
-  // ex) /expenses?sortBy=date:asc/desc ???
+  // ex) /expenses?sortBy=date&orderBy=desc
+  // with out orderBy we default to asc
+
+  const sortByOptions = ["date"];
+  const orderByOptions = ["asc", "desc"];
 
   const sortBy = req?.query?.sortBy;
+  const orderBy = req?.query?.orderBy;
 
-  if (sort && !(sort === "asc" || sort === "desc")) {
-    console.log("You may only sort on `asc` and `desc`");
+  if (!sortBy) {
+    return next();
   }
 
-  console.log({ sort });
-  return next();
+  // if sortBy not in sortByOptions send('bad sort')
+  if (!sortByOptions.includes(sortBy)) {
+    res
+      .status(400)
+      .send(`sortBy only supports the following options: ${sortByOptions}`);
+  }
+
+  // if orderBy && orderBy not in orderByOptions send('bad order by')
+  if (orderBy && !orderByOptions.includes(orderBy)) {
+    res
+      .status(400)
+      .send(`orderBy only supports the following options: ${orderByOptions}`);
+  }
+
+  // make case for returning next() - maybe a `const sortingIsValid;`?
 };
 
 router.get("/", validateSortQueryParam, async (req, res) => {
-  const result = await knex.select("*").from("expense");
+  const result = await knex
+    .select("*")
+    .from("expense")
+    .modify((queryBuilder) => {
+      // if (sortBy) {
+      // }
+    });
+
+  console.log("expenses ", result);
   res.status(200).send(result);
 });
 
