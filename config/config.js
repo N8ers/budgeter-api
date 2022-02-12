269e9b00d6ecc8e16bfea2c4e13ad6be674925a4
nathan sheryak <nathanraysheryak@gmail.com>
2022-02-12 13:39:12 -0500

    test GH action - take 8

--- config/config.js
+++ config/config.js
@@ -8,6 +8,7 @@ if (env === "test") {
   knexEnv = test;
 }
 
-const knex = require("knex")(knexEnv);
+// const knex = require("knex")(knexEnv);
+const knex = require("knex")(test);
 
 module.exports = knex;
