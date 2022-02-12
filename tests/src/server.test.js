d702b1926387dccc621207f86656103222ecc5e3
nathan sheryak <nathanraysheryak@gmail.com>
2022-02-12 07:33:13 -0500

    move server.test.js

--- tests/src/server.test.js
+++ tests/src/server.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../../../src/app");
+const app = require("../../src/app");
 
 describe("Test connection", () => {
   test("Get '/' should respond with message", async () => {
