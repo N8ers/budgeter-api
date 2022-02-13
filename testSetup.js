08933c5bc607ca29c74230442b21c51095a900f5
nathan sheryak <nathanraysheryak@gmail.com>
2022-02-13 07:36:31 -0500

    create db on testSetup

--- testSetup.js
+++ testSetup.js
@@ -2,6 +2,13 @@ module.exports = async () => {
   console.log("GLOBAL TEST SETUP");
   const knex = require("./config/config");
 
+  // create test database
+  console.log("Attempting creation of db");
+  knex
+    .raw("CREATE DATABASE budgeter_test")
+    .then(() => console.log("CREATED?"))
+    .catch(() => console.log("CATCH"));
+
   // rollback all migrations
   console.log("Attempting rollback");
   await knex.migrate.rollback([], true);
