87d350ad507fadd5c9188dbdf7b34ef056a408e5
nathan sheryak <nathanraysheryak@gmail.com>
2022-02-13 07:48:16 -0500

    fix script

--- ci-create-db.js
+++ ci-create-db.js
@@ -1,6 +1,9 @@
 const knex = require("./config/config");
 
-// create test database
-console.log("Attempting creation of db");
-await knex.raw("CREATE DATABASE IF NO EXISTS budgeter_test");
-console.log("DB created");
+const createDB = async function () {
+  console.log("Attempting creation of db");
+  await knex.raw("CREATE DATABASE IF NO EXISTS budgeter_test");
+  console.log("DB created");
+};
+
+await createDB();
