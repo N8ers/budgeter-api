9305d09f4e74ca685e8d23d2d02e45e4214a9c80
nathan sheryak <nathanraysheryak@gmail.com>
2022-02-17 06:32:30 -0500

    write schema tests

--- tests/src/routes/user.routes.test.js
+++ tests/src/routes/user.routes.test.js
@@ -1,6 +1,8 @@
 const request = require("supertest");
 const app = require("../../../src/app");
 
+const nameWithOneHundredAndOneChars =
+  "JhpWY7DSfFQLU9HByQeXWLEBj96BmdeAzaNzqEBfQgReHbtfqVJOnyyPo1VlwbNttoHvJwrGSWXdWwp48AkQrw5pzQeY8W8sHnQ93";
 describe("User", () => {
   describe("CRUD", () => {
     test("Create", async () => {
@@ -78,7 +80,7 @@ describe("User", () => {
         .get(`/users/${postResponseId}`)
         .expect(500);
       expect(getResponse.body).toStrictEqual({
-        message: `User ${postResponseId}, may not exist.`,
+        message: `User ${postResponseId} may not exist.`,
       });
     });
   });
@@ -103,6 +105,135 @@ describe("User", () => {
 
       expect(getResponse.body).toStrictEqual({
         message: `User ${userIdThatProbablyDoesNotExist} may not exist.`,
+      });
+    });
+  });
+
+  describe("Schema", () => {
+    describe("POST", () => {
+      test("Name field should be string", async () => {
+        const response = await request(app)
+          .post("/users")
+          .send({ name: 86593 })
+          .expect(422);
+
+        expect(response.body).toStrictEqual({
+          data: {
+            name: 86593,
+          },
+          message: '"name" must be a string',
+          status: "error",
+        });
+      });
+      test("Name field should have min 3 characters", async () => {
+        const response = await request(app)
+          .post("/users")
+          .send({ name: "jo" })
+          .expect(422);
+
+        expect(response.body).toStrictEqual({
+          data: {
+            name: "jo",
+          },
+          message: '"name" length must be at least 3 characters long',
+          status: "error",
+        });
+      });
+      test("Name field should have max 100 characters", async () => {
+        const response = await request(app)
+          .post("/users")
+          .send({ name: nameWithOneHundredAndOneChars })
+          .expect(422);
+
+        expect(response.body).toStrictEqual({
+          data: {
+            name: nameWithOneHundredAndOneChars,
+          },
+          message:
+            '"name" length must be less than or equal to 100 characters long',
+          status: "error",
+        });
+      });
+      test("Name field is required", async () => {
+        const response = await request(app).post("/users").send({}).expect(422);
+
+        expect(response.body).toStrictEqual({
+          data: {},
+          message: '"name" is required',
+          status: "error",
+        });
+      });
+    });
+
+    describe("PUT", () => {
+      let exampleResponseId;
+      beforeAll(async () => {
+        let exampleResponse = await request(app)
+          .post("/users")
+          .send({ name: "Tsuki the cat" })
+          .expect(200);
+        exampleResponseId = exampleResponse.body.id;
+      });
+
+      test("Name field should be string", async () => {
+        const response = await request(app)
+          .put("/users")
+          .send({ id: exampleResponseId, name: 86593 })
+          .expect(422);
+
+        expect(response.body).toStrictEqual({
+          data: {
+            id: exampleResponseId,
+            name: 86593,
+          },
+          message: '"name" must be a string',
+          status: "error",
+        });
+      });
+      test("Name field should have min 3 characters", async () => {
+        const response = await request(app)
+          .put("/users")
+          .send({ id: exampleResponseId, name: "jo" })
+          .expect(422);
+
+        expect(response.body).toStrictEqual({
+          data: {
+            id: exampleResponseId,
+            name: "jo",
+          },
+          message: '"name" length must be at least 3 characters long',
+          status: "error",
+        });
+      });
+      test("Name field should have max 100 characters", async () => {
+        const response = await request(app)
+          .put("/users")
+          .send({ id: exampleResponseId, name: nameWithOneHundredAndOneChars })
+          .expect(422);
+
+        expect(response.body).toStrictEqual({
+          data: {
+            id: exampleResponseId,
+            name: nameWithOneHundredAndOneChars,
+          },
+          message:
+            '"name" length must be less than or equal to 100 characters long',
+          status: "error",
+        });
+      });
+      test("Name field is required", async () => {
+        const response = await request(app)
+          .put("/users")
+          .send({ id: exampleResponseId })
+          .expect(422);
+
+        expect(response.body).toStrictEqual({
+          data: {
+            id: exampleResponseId,
+          },
+          message: '"name" is required',
+          status: "error",
+        });
       });
     });
   });
