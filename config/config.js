23e21685e6c9a9a048d5a8e61aa14833fbf496a2
nathan sheryak <nathanraysheryak@gmail.com>
2022-02-19 07:48:20 -0500

    move dotenv config to app.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const { development, test, ciTest } = require("../knexfile");
 
 const env = process.env.NODE_ENV;
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+require("dotenv").config();
 
 const routes = {
   users: require("./routes/user.routes/index"),
