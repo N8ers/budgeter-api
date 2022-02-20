176051bddbc32b32839ddc30c750687618a6a7f4
nathan sheryak <nathanraysheryak@gmail.com>
2022-02-20 06:46:49 -0500

    jest setup/teardown to use pgm

--- tearDown.js
+++ tearDown.js
@@ -1,1 +1,19 @@
-module.exports = async () => {};
+const { execSync } = require("child_process");
+
+module.exports = async () => {
+  // find a better way to do this
+  // probably reach into the migrations folder, cound files
+  // then run function for each file?
+
+  const migrationFiles = [
+    { migrationName: "1645137521808_create-users" },
+    { migrationName: "1645140814791_create-vendors" },
+    { migrationName: "1645140904704_create-categories" },
+    { migrationName: "1645140941598_create-expenses" },
+  ];
+
+  for (file of numberOfMigrationFiles) {
+    console.log("rolling back ", migrationFiles[file]);
+    await execSync("npm run migrate down");
+  }
+};
--- testSetup.js
+++ testSetup.js
@@ -1,1 +1,6 @@
-module.exports = async () => {};
+const { execSync } = require("child_process");
+
+module.exports = async () => {
+  console.log("process.env.NODE_ENV ", process.env.NODE_ENV);
+  execSync("npm run migrate up");
+};
