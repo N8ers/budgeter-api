0899ad0be37d8fbf00fa33dc00f665544ba7921e
nathan <nathan.sheryak@wwt.com>
2022-02-28 06:16:01 -0500

    DRY up schemaValidation

--- src/middleware/schemaValidation.js
+++ src/middleware/schemaValidation.js
@@ -1,9 +1,11 @@
 const userSchema = require("../schemas/user.schema");
 const categorySchema = require("../schemas/category.schema");
+const vendorSchema = require("../schemas/vendor.schema");
+const expenseSchema = require("../schemas/expense.schema");
 
-const validateUserSchema = async function (req, res, next) {
+const generateSchema = async function (req, res, next, schema) {
   const body = req.body;
-  const { error, value } = userSchema.validate(body);
+  const { error, value } = schema.validate(body);
 
   if (error) {
     const errors = error.details.map((error) => error.message).join(". ");
@@ -13,19 +15,24 @@ const validateUserSchema = async function (req, res, next) {
   }
 };
 
-const validateCategorySchema = async function (req, res, next) {
-  const body = req.body;
-  const { error, value } = categorySchema.validate(body);
+const validateUserSchema = async function (req, res, next) {
+  await generateSchema(req, res, next, userSchema);
+};
 
-  if (error) {
-    const errors = error.details.map((error) => error.message).join(". ");
-    res.status(422).json({ status: "error", message: errors, data: value });
-  } else {
-    return next();
-  }
+const validateCategorySchema = async function (req, res, next) {
+  await generateSchema(req, res, next, categorySchema);
+};
+
+const validateVendorSchema = async function (req, res, next) {
+  await generateSchema(req, res, next, vendorSchema);
+};
+const validateExpenseSchema = async function (req, res, next) {
+  await generateSchema(req, res, next, expenseSchema);
 };
 
 module.exports = {
   validateUserSchema,
   validateCategorySchema,
+  validateVendorSchema,
+  validateExpenseSchema,
 };
