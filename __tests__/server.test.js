15f367442839b1a7ca6412aa1a937deadbc33211
nathan sheryak <nathanraysheryak@gmail.com>
2022-03-28 07:44:41 -0400

    start moving tests into __tests__

---
+++ __tests__/server.test.js
@@ -0,0 +1,15 @@
+const request = require("supertest");
+const app = require("../dist/src/app");
+
+describe("Test connection", () => {
+  test("Get '/' should respond with message", async () => {
+    const response = await request(app).get("/");
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe("new setup works");
+  });
+  test("Non matching route returns appropriate message", async () => {
+    const response = await request(app).get("/aRouteThatDoesNotExist");
+    expect(response.statusCode).toBe(404);
+    expect(response.text).toBe("no such route exists");
+  });
+});
